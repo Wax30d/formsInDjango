24be66008505e8aea50e9eddf393a4492f9b7f86
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-02-05 13:39:36 +0500

    Created a view function for SignUp form.

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+
+def regForm(request):
+    form = forms.SignUp()
+    if request.method == 'POST':
+        form = forms.SignUp(request.POST)
+        html = 'we have received this form again'
+    else:
+        html = 'welcome for first time'
+    return render(request, 'signup.html', {'html': html, 'form': form})
